1dd01525e59c505699974dc3f51a1c12f100934b
jmoralesv <korum03@gmail.com>
2023-09-15 20:42:46 -0500

    Another small test.

--- src/Market.Comparison.Api/Program.cs
+++ src/Market.Comparison.Api/Program.cs
@@ -23,11 +23,11 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 10).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 80),
+            Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
