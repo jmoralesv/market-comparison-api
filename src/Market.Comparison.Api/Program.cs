01d305faa45bdcd321cab915df1734a6d9344082
jmoralesv <korum03@gmail.com>
2023-10-07 00:45:21 -0500

    Refactors for configuring the services, the pipeline middlewares and the endpoints.

--- src/Market.Comparison.Api/Program.cs
+++ src/Market.Comparison.Api/Program.cs
@@ -1,107 +1,11 @@
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Market.Comparison.Api;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
-        new OpenApiSecurityScheme
-        {
-            Name = Microsoft.Net.Http.Headers.HeaderNames.Authorization,
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = JwtBearerDefaults.AuthenticationScheme,
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "JSON Web Token based security",
-        });
-    options.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = JwtBearerDefaults.AuthenticationScheme
-                    }
-                },
-                Array.Empty<string>()
-            }
-        });
-});
-
-// Accepts any access token issued by identity server
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-    {
-        options.Authority = builder.Configuration["Jwt:Authority"];
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateAudience = false
-        };
-    });
-
-// adds an authorization policy to make sure the token is for scope 'api1'
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("ApiScope", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-        policy.RequireClaim("scope", "Market.Comparison.Api");
-    });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeline();
 
 // Register the endpoints
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", (HttpContext context) =>
-{
-    var user = context.User;
-
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)],
-            user.Claims.Select(c => (claimType: c.Type, claimValue: c.Value)).ToList()
-        ))
-        .ToArray();
-    return forecast;
-})
-.RequireAuthorization("ApiScope")
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapWeatherForecastEndpoints();
 
 await app.RunAsync();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, List<(string claimType, string claimValue)> claims)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
