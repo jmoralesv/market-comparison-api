2e57738fcc9404af41b4cc74fd3a8d1f03d9d19b
Jorge Morales <korum03@gmail.com>
2023-10-07 21:33:59 -0500

    Adding api access (#3)
    * Refactors for configuring the services, the pipeline middlewares and the endpoints.
    
    * Adding Swagger documentation for the weather forecast endpoint using the Produces methods.

---
+++ src/Market.Comparison.Api/HostingExtensions.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Market.Comparison.Api;
+
+internal static class HostingExtensions
+{
+    internal static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+                new OpenApiSecurityScheme
+                {
+                    Name = Microsoft.Net.Http.Headers.HeaderNames.Authorization,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JSON Web Token based security",
+                });
+            options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+        });
+
+        // Accepts any access token issued by identity server
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.Authority = builder.Configuration["Jwt:Authority"];
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false
+                };
+            });
+
+        // adds an authorization policy to make sure the token is for scope 'Market.Comparison.Api'
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy("ApiScope", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim("scope", "Market.Comparison.Api");
+            });
+        });
+
+        return builder.Build();
+    }
+
+    internal static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        return app;
+    }
+}
---
+++ src/Market.Comparison.Api/WeatherForecastEndpoints.cs
@@ -0,0 +1,50 @@
+﻿namespace Market.Comparison.Api;
+
+internal static class WeatherForecastEndpoints
+{
+    internal static WebApplication MapWeatherForecastEndpoints(this WebApplication app)
+    {
+        var groupBuilder = app.MapGroup("weather-forecast");
+        groupBuilder.MapGetWeatherForecastEndpoint();
+        return app;
+    }
+
+    private static RouteGroupBuilder MapGetWeatherForecastEndpoint(this RouteGroupBuilder groupBuilder)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        groupBuilder.MapGet("/", (HttpContext context) =>
+        {
+            var user = context.User;
+
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)],
+                    user.Claims.Select(c => new Claim(c.Type, c.Value)).ToList()
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .RequireAuthorization("ApiScope")
+        .WithName("GetWeatherForecast")
+        .WithOpenApi()
+        .Produces<WeatherForecast[]>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound)
+        .Produces(StatusCodes.Status401Unauthorized);
+
+        return groupBuilder;
+    }
+}
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, List<Claim> claims)
+{
+    internal int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal record Claim(string ClaimType, string ClaimValue);
